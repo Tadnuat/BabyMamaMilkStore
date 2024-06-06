36137aa8db8f3b97feb77a684e5a355eafd38aea
Tadnuat <datdptse172916@fpt.edu.vn>
2024-06-06 22:57:25 +0700

    API admin

--- Milk/MilkRepo/obj/Debug/net8.0/MilkStore.Repo.AssemblyInfo.cs
+++ Milk/MilkRepo/obj/Debug/net8.0/MilkStore.Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ad825851f00166bc8f1aaed7dfaacbf7c1df03")]
 [assembly: System.Reflection.AssemblyProductAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
