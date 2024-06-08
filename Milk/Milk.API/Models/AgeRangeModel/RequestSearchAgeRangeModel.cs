ec7570405aef3401dad96f4ef046c6f149d33057
HaDucBao21103 <ducbao21103@gmail.com>
2024-06-08 15:50:38 +0700

    4 entity

---
+++ Milk/Milk.API/Models/AgeRangeModel/RequestSearchAgeRangeModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MilkStore.API.Models.AgeRangeModel
+{
+    public class RequestSearchAgeRangeModel
+    {
+        public string? Baby { get; set; }
+        public string? Mama { get; set; }
+        public int? ProductId { get; set; }
+        public int PageIndex { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+        public SortContent SortContent { get; set; }
+    }
+
+    public class SortContent
+    {
+        public string SortBy { get; set; }
+        public SortTypeEnum SortType { get; set; }
+    }
+
+    public enum SortTypeEnum
+    {
+        Ascending,
+        Descending
+    }
+}
---
+++ Milk/Milk.API/Models/CompanyModel/RequestSearchCompanyModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MilkStore.API.Models.CompanyModel
+{
+    public class RequestSearchCompanyModel
+    {
+        public string? CompanyName { get; set; }
+        public int? CountryId { get; set; }
+        public int PageIndex { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+        public SortContent SortContent { get; set; }
+    }
+
+    public class SortContent
+    {
+        public string SortBy { get; set; }
+        public SortCompanyTypeEnum SortCompanyType { get; set; }
+    }
+
+    public enum SortCompanyTypeEnum
+    {
+        Ascending,
+        Descending
+    }
+}
