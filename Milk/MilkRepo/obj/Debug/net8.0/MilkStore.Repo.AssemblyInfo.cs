18adf1e04c6210f14a3cd0488d0cfe07eb530846
HaDucBao21103 <ducbao21103@gmail.com>
2024-06-13 13:53:14 +0700

    update 13/6

--- Milk/MilkRepo/obj/Debug/net8.0/MilkStore.Repo.AssemblyInfo.cs
+++ Milk/MilkRepo/obj/Debug/net8.0/MilkStore.Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f70a13c50324c1a5b59374ff8df001ab42aee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1952befb07ea6fd3e3166c1db87638da21aefb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MilkStore.Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
